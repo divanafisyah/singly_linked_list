299644a5af99995f4d6788db757fdeeb527a1a15
Diva Nafisyah <diva.nafisyah.ft21@mail.umy.ac.id>
2022-11-07 17:20:35 +0700

    singly linked list

--- singly_linked_list/Program.cs
+++ singly_linked_list/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -32,6 +33,7 @@ namespace singly_linked_list
             Node newnode = new Node();
             newnode.rollNumber = rollNo;
             newnode.name = nm;
+            //if the node to be inserted is the first node
             if(START == null || rollNo <= START.rollNumber)
             {
                 if((START != null) && (rollNo == START.rollNumber))
@@ -42,8 +44,45 @@ namespace singly_linked_list
                 newnode.next = START;
                 START = newnode;
                 return;
-
             }
+            Node previous, current;
+            previous = START;
+            current = START;
+
+            while ((current != null) && (rollNo >= current.rollNumber))
+            {
+                if (rollNo == current.rollNumber)
+                {
+                    Console.WriteLine();
+                    return;
+                }
+                previous.next = current;
+                previous.next = newnode;
+            }
+            previous.next = current;
+            previous.next = newnode;
+        }
+        public bool delNode(int rollNo)
+        {
+            Node previous, current;
+            previous = current = null;
+            if (Search(rollNo, ref previous, ref current) == false)
+                return false;
+                if (current == START)
+                    START = START.next;
+            return true;
+        }
+
+        public bool Search(int rollNo, ref  Node previous, ref Node current)
+        {
+            previous = START;
+            current = START;
+            while((current != null)&&(rollNo != current.rollNumber))
+            {
+                previous = current;
+                current = current.next;
+            }
+        }
         }
 
     }
