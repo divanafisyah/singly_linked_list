d42ce3e42702bc6acc2bf905edf6c56a75641bb2
Diva Nafisyah <diva.nafisyah.ft21@mail.umy.ac.id>
2022-11-07 16:48:42 +0700

    Add project files.

---
+++ singly_linked_list/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace singly_linked_list
+{
+    //each Node consist of the information part and link to the next node
+    class Node
+    {
+        public int rollNumber;
+        public string name;
+        public Node next;
+    }
+
+    class List
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
