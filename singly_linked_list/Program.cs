edc59330ec086af89c9bae287cfb78799be17d16
Diva Nafisyah <diva.nafisyah.ft21@mail.umy.ac.id>
2022-11-07 17:02:28 +0700

    singly linked list

--- singly_linked_list/Program.cs
+++ singly_linked_list/Program.cs
@@ -16,6 +16,35 @@ namespace singly_linked_list
 
     class List
     {
+        Node START;
+        public List()
+        { 
+            START = null;
+        }
+        public void addNote()
+        {
+            int rollNo;
+            string nm;
+            Console.Write("\nEnter the roll number of the student:");
+            rollNo = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the roll number of the student:");
+            nm = Console.ReadLine();
+            Node newnode = new Node();
+            newnode.rollNumber = rollNo;
+            newnode.name = nm;
+            if(START == null || rollNo <= START.rollNumber)
+            {
+                if((START != null) && (rollNo == START.rollNumber))
+                {
+                    Console.WriteLine();
+                    return;
+                }
+                newnode.next = START;
+                START = newnode;
+                return;
+
+            }
+        }
 
     }
     class Program
